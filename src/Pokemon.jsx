8f72bb9151c350a97eb2978db94183d1fea69e83
Tanay Bhor <bhortanay@gmail.com>
2025-05-15 22:20:52 +0530

    second commit

---
+++ src/Pokemon.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+export const Pokemon = () => {
+
+
+    return(
+        <>
+            <section className='container'>
+
+                <header>
+                    <h1>Let's catch Pokémon</h1>
+                </header>
+
+            </section>
+        </>
+    )
+}
