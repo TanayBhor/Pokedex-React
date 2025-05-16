4e332012de1dd5cce160387b14c7351c69fb18a6
Tanay Bhor <bhortanay@gmail.com>
2025-05-16 15:48:25 +0530

    API fetched

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react'
 import './index.css'
 
 export const Pokemon = () => {
 
+    const[pokemon, setPokemon] = useState([])
+    const[loading, setLoading] = useState(true)
+    const[error, setError] = useState(null)
+
+    const API = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+
+    async function fetchPokemon(){
+        try {
+            const res = await fetch(API)
+            const data = await res.json()
+
+            const detailedPokemonData = data.results.map(async (curPokemon)=>{
+                const res = await fetch(curPokemon.url)
+                const data = await res.json()
+
+                return data;
+            })
+
+            const detailedResponses = await Promise.all(detailedPokemonData);
+            console.log(detailedResponses);
+            setPokemon(detailedResponses)
+            setLoading(false)
+            
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+            setError(error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchPokemon()
+    },[])
 
     return(
         <>
@@ -9,6 +43,12 @@ export const Pokemon = () => {
 
                 <header>
                     <h1>Let's catch Pokémon</h1>
+
+                    <div>{pokemon.map((e)=>{
+                        return(
+                            <img src={e.sprites.other.dream_world.front_default} alt="" />
+                        )
+                    })}</div>
                 </header>
 
             </section>
